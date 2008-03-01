ccf41a711ce8d57b30f4203bd350e85c9aeb013d
William Xu <william.xwl@gmail.com>
2008-03-01 16:39:00 +0000

    A complete rewritten, now as a minor mode.
    Bump to version 3.0.
    
    darcs-hash:20080301163959-cfa61-7f4dcbe160a2c5ac673b99fc0c3d48097d07bf96.gz

--- smart-operator.el
+++ smart-operator.el
@@ -1,194 +1,163 @@
-;;; smart-operator.el --- Insert operators packed with whitespaces smartly
+;;; smart-operator.el --- Insert operators with surrounding spaces smartly
 
-;; Copyright (C) 2004, 2005, 2007 William Xu
+;; Copyright (C) 2004, 2005, 2007, 2008 William Xu
 
 ;; Author: William Xu <william.xwl@gmail.com>
-;; Version: 0.9
+;; Version: 1.0
+;; Url: http://williamxu.net9.org/ref/smart-operator.el
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;; along with EMMS; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
-;; This package can automatically add whitespaces before and after operators.
-;; e.g, "=" becomes " = ", "+=" becomes " += ". which will be handy for
-;; writing C-style sources.
+;; When typing operators, this package can automatically insert spaces
+;; before and after operators. For instance, `=' will become ` = ', `+='
+;; will become ` += '. This is handy for writing C-style sources.
 
-;; To use, set all the operators smart at one time, then ajust some at
-;; your wish e.g.
+;; To use, put this file to your load-path and the following to your
+;; ~/.emacs:
+;;             (require 'smart-operator)
 ;;
-;; (defun my-c-mode-common-hook()
-;;   (smart-insert-operator-hook)
-;;
-;;   (local-unset-key (kbd "."))
-;;   (local-unset-key (kbd ":"))
-;;   (local-set-key (kbd "*") 'c-electric-star))
-;;
-;; (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+;; Then `M-x smart-operator-mode' for toggling this minor mode.
 
-;; Besides `smart-insert-operator' when writing, there's also
-;; `smart-beautify-operator' to beautify ugly codes, be careful there
-;; are still some unresolved issues in `smart-beautify-operator'!
+;;; Acknowledgements
 
-;; Put this file into your load-path and the following into your ~/.emacs:
-;;   (require 'smart-operator)
-
-;;; Todo:
-
-;; - Some unresolved issues in `smart-beautify-operator', such as:
-;;   printf("%s")
-;; - Clean fun/var names with respect to package name
+;; Nikolaj Schumacher <n_schumacher@web.de>, for suggesting
+;; reimplementing as a minor mode and providing an initial patch for
+;; that.
 
 ;;; Code:
 
-(defvar smart-operator-alist
-  '( "=" "<" ">" "%" "\\+" "-" "\\*" "/" "&" "|" "!" ":"))
+;;; smart-operator minor mode
+
+(defvar smart-operator-mode-map
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap "=" 'smart-operator-self-insert-command)
+    (define-key keymap "<" 'smart-operator-<)
+    (define-key keymap ">" 'smart-operator-self-insert-command)
+    (define-key keymap "%" 'smart-operator-self-insert-command)
+    (define-key keymap "+" 'smart-operator-self-insert-command)
+    (define-key keymap "-" 'smart-operator-self-insert-command)
+    (define-key keymap "*" 'smart-operator-self-insert-command)
+    (define-key keymap "/" 'smart-operator-self-insert-command)
+    (define-key keymap "&" 'smart-operator-self-insert-command)
+    (define-key keymap "|" 'smart-operator-self-insert-command)
+    (define-key keymap "!" 'smart-operator-self-insert-command)
+    (define-key keymap ":" 'smart-operator-:)
+    (define-key keymap "?" 'smart-operator-self-insert-command)
+    (define-key keymap "," 'smart-operator-,)
+    (define-key keymap "." 'smart-operator-.)
+    keymap)
+  "Keymap used my `smart-operator-mode'.")
 
 ;;;###autoload
-(defun smart-insert-operator (op &optional only-back)
-  "Automatically insert whitespaces before and after '=', '>=', etc.
-Make it look nicer: ' = ', ' >= '.
+(define-minor-mode smart-operator-mode
+  "Insert operators with surrounding spaces smartly."
+  nil "_+_ " smart-operator-mode-map)
 
-OP is the operator. If optional ONLY-BACK is t, only insert one
-whitespace at back. When confused, try C-q."
-  (interactive "s")
+(defun smart-operator-mode-on ()
+  (smart-operator-mode 1))
+
+;;;###autoload
+(defun smart-operator-self-insert-command (arg)
+  "Insert the entered operator plus surrounding spaces."
+  (interactive "p")
+  (smart-operator-insert (string last-command-char)))
+
+(defvar smart-operator-list
+  '("=" "<" ">" "%" "+" "-" "*" "/" "&" "|" "!" ":" "?" "," "."))
+
+(defun smart-operator-insert (op &optional only-after)
+  "Insert operator OP with surrounding spaces.
+e.g., `=' will become ` = ', `+=' will become ` += '.
+
+When ONLY-AFTER, insert space at back only."
   (delete-horizontal-space)
-  (let ((op-p nil)			; another op at front?
-	(one-char-back nil))		; have we stepped back one char?
-    (unless (bolp)
-      (backward-char)
-      (setq one-char-back t))
-    (setq op-p
-	  (catch 'return
-	    (dolist (front smart-operator-alist)
-	      (when (looking-at front)
-		(throw 'return t)))))
-    (when (and (or op-p (not (and (bolp) (eolp))))
-	       one-char-back)
-      (forward-char))
-    (if (or op-p only-back (bolp))
-	(insert op)
-      (insert (concat " " op))))
-  (delete-horizontal-space)
-  (backward-char)                ; indent the symbol if it's at the bol.
-  (when (bolp)
-    (indent-according-to-mode))
-  (forward-char)
-  (insert " "))
+  (if (or (looking-back (regexp-opt smart-operator-list)
+                        (save-excursion (beginning-of-line)
+                                        (point)))
+          only-after
+          (bolp))
+      (progn (insert (concat op " "))
+             (save-excursion
+               (backward-char 2)
+               (when (bolp)
+                 (indent-according-to-mode))))
+    (insert (concat " " op " "))))
 
-(defun smart-operator-replace-regexp (regexp to-string &optional enhance)
-  "Faster and won't set the mark or print anything.
+;; fine tunings
 
-If optional enhance is non-nil, call replace-regexp for its \& and \D
-functionity."
-  (save-excursion
-    (goto-char (point-min))
-    (if enhance
-	(replace-regexp regexp to-string nil)
-      (while (re-search-forward regexp nil t)
-	(replace-match to-string nil t)))))
+(defun smart-operator-< ()
+  "See `smart-operator-insert'."
+  (interactive)
+  (cond ((memq major-mode '(c-mode c++-mode))
+         (smart-operator-c-mode-<))
+        (t
+         (call-interactively 'smart-operator-self-insert-command))))
 
-(defun smart-operator-replace (from to)
-  "Faster and won't set the mark or print anything."
-  (save-excursion
-    (goto-char (point-min))
-    (while (search-forward from nil t)
-      (replace-match to nil t))))
+(defun smart-operator-: ()
+  "See `smart-operator-insert'."
+  (interactive)
+  (cond ((memq major-mode '(c-mode c++-mode))
+         (smart-operator-c-mode-:))
+        (t
+         (smart-operator-insert ":" t))))
 
-;;;###autoload
-(defun smart-beautify-operator (beg end)
-  "Beautify the codes to my style, such as add whitespaces before
-and after operators. Three steps:
+(defun smart-operator-, ()
+  "See `smart-operator-insert'."
+  (interactive)
+  (smart-operator-insert "," t))
 
-    1.\" =\", \"= \" --> \" = \"
-    2.\"  \" --> \" \"
-    3.\"> =\" --> \">=\"
-
-Note: As replace method has been called two times, so you have to undo
-TWO times to get back to the original state! And be careful to mark whole
-buffer, as there are some unresolved issues, such as \"#include <stdio.h>\"
-would become \" #include < stdio.h > \" incorrectly!"
-  (interactive "r")
-  (save-excursion
-    (narrow-to-region beg end)
-    (message "beautifying operators...")
-    (mapcar* '(lambda (from) (smart-operator-replace from (concat " " from " ")))
-	     '("=" ">" "<" "+" "-" "*" "/" "%" "|" "&" "!" ","))
-    (smart-operator-replace-regexp "\\ +" " " t)
-    (mapcar* '(lambda (from to) (smart-operator-replace from to))
-	     '("> =" "< =" "= =" "+ +" "- -" "+ =" "- =" "* =" "/ =" "% ="
-	       "| |" "& &" "! !" "| =" "& =" "! ="
-	       " ," " / /" " / *" "* / " "* *" "< <")
-	     '(">=" "<=" "==" "++" "--" "+=" "-=" "*=" "/=" "%="
-	       "||" "&&" "!!" "|=" "&=" "!="
-	       "," "//" "/*" "*/" "**" "<<"))
-    (indent-region beg end nil)
-    (message "beautifying operators...done")
-    (widen)))
-
-;;;###autoload
-(defun smart-insert-operator-hook ()
-"Set all the operators smart at one time, then you can ajust some by hand,
-e.g.
-
-\(defun my-c-mode-common-hook()
-  (smart-insert-operator-hook)
-
-  (local-unset-key (kbd \".\"))
-  (local-unset-key (kbd \":\"))
-  (local-set-key (kbd \"*\") 'c-electric-star))
-
-\(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)"
-
-  (local-set-key (kbd "=") (lambda () (interactive) (smart-insert-operator "=")))
-  (local-set-key (kbd "+") (lambda () (interactive) (smart-insert-operator "+")))
-  (local-set-key (kbd "-") (lambda () (interactive) (smart-insert-operator "-")))
-  (local-set-key (kbd "/") (lambda () (interactive) (smart-insert-operator "/")))
-  (local-set-key (kbd "%") (lambda () (interactive) (smart-insert-operator "%")))
-  (local-set-key (kbd "&") (lambda () (interactive) (smart-insert-operator "&")))
-  (local-set-key (kbd "*") (lambda () (interactive) (smart-insert-operator "*")))
-  (local-set-key (kbd "!") (lambda () (interactive) (smart-insert-operator "!")))
-  (local-set-key (kbd "|") (lambda () (interactive) (smart-insert-operator "|")))
-  (local-set-key (kbd "<") (lambda () (interactive) (smart-insert-operator "<")))
-  (local-set-key (kbd ">") (lambda () (interactive) (smart-insert-operator ">")))
-  (local-set-key (kbd ",") (lambda () (interactive) (smart-insert-operator "," t)))
-  (local-set-key (kbd ".") (lambda () (interactive) (smart-insert-operator "." t)))
-;; (local-set-key (kbd "\;") (lambda () (interactive) (smart-insert-operator "\;" t)))
-  (local-set-key (kbd ":") (lambda () (interactive) (smart-insert-operator ":" t))))
+(defun smart-operator-. ()
+  "See `smart-operator-insert'."
+  (interactive)
+  (cond ((looking-back "[0-9]" (1- (point)))
+         (insert "."))
+        (t (smart-operator-insert "." t)
+           (insert " "))))
 
 
-;;; C & C++
+;;; c, c++
 
-;;;###autoload
-(defun smart-insert-c-style-< ()
-  "Insert `<>' or ` < ' smartly.
-If there are some keywords(like #include, vector) ahead on the same
-line, possibly we have to insert a `<>' instead of ` < '."
+(defun smart-operator-c-mode-< ()
+  "Insert `<>' or ` < '.
+
+If there are some preceding keywords, like #include, vector,
+probably we want to insert `<>'."
   (interactive)
-  (if (save-excursion
-        (re-search-backward
-         (regexp-opt '("#include" "vector" "deque" "list" "map" "multimap"
-                       "set" "hash_map" "iterator" "template" "pair"
-                       "auto_ptr"))
-         (save-excursion
-           (beginning-of-line)
-           (point))
-         t
-         1))
+  (if (looking-back
+       (concat "\\("
+               (regexp-opt
+                '("#include" "vector" "deque" "list" "map"
+                  "multimap" "set" "hash_map" "iterator" "template"
+                  "pair" "auto_ptr"))
+               "\\)\\ *")
+       (save-excursion (beginning-of-line)
+                       (point)))
       (progn (insert "<>")
              (backward-char))
-    (smart-insert-operator "<")))
+    (smart-operator-insert "<")))
+
+(defun smart-operator-c-mode-: ()
+  "Insert scope symbol `::' or ` : ' as part of ? operator."
+  (interactive)
+  (if (looking-back "\\?.+" (save-excursion (beginning-of-line)
+                                            (point)))
+      (smart-insert-operator ":")
+    (insert ":")))
 
 (provide 'smart-operator)
 
