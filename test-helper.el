c455e4ffac864486a8ab25ca9d418f37e98138df
David Nikel-Shepherd <davidshepherd7@gmail.com>
2025-05-24 16:54:19 +0000

    Simplify import paths

--- test-helper.el
+++ test-helper.el
@@ -1,5 +1,4 @@
 (require 'ert)
-(require 'electric-operator)
 (require 'edmacro)
 
 ;; Don't load old byte-compiled versions!
@@ -7,19 +6,12 @@
 
 (require 'f)
 
-(defvar electric-operator-test-path
-  (f-dirname load-file-name))
-
-(defvar electric-operator-root-path
-  (f-parent electric-operator-test-path))
-
-(add-to-list 'load-path electric-operator-root-path)
-
-
 (defun log-trace-buffer (&rest _)
   (when (get-buffer trace-buffer)
     (with-current-buffer trace-buffer
       (message (buffer-substring-no-properties (point-min) (point-max))))))
+
+(defvar ert-runner-reporter-run-ended-functions) ;; shut up the byte commpiler
 (add-to-list 'ert-runner-reporter-run-ended-functions #'log-trace-buffer)
 
 ;;; Test helper functions for integration tests
